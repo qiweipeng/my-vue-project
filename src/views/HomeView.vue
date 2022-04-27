f7098c0262e8e0e898d7e0a44c9e803cf509e0e3
Weipeng Qi <qiweipeng@hotmail.com>
2022-04-27 17:42:54 +0800

    删除样板代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <p>Home</p>
+  </div>
 </template>
